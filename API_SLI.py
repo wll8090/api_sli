715785df2e2790f90fee2b8999639a7a47da929f
wll8090 <wllyvn@gmail.com>
2023-08-15 11:18:44 -0300

    inicialização de versionamento da API-SLI

---
+++ API_SLI.py
@@ -0,0 +1,269 @@
+from flask import Flask ,abort, jsonify, request, make_response,render_template
+from hashlib import sha256
+from datetime import datetime
+from flask_cors import CORS
+from clildap import user_ldap
+from datetime import datetime, timedelta
+from functools import wraps
+
+import json
+import logging
+
+with open('config.ini') as arq:
+    conf=json.loads(arq.read())
+    arq.close()
+
+port=conf.get('init').get('PORT')
+host=conf.get('init').get('HOST')
+ipCORS=conf.get('init').get('IPCORS')
+logs=conf.get('testes').get('LOGS')
+liberado=conf.get('testes').get('LIBERADO')
+val_tokem=conf.get('testes').get('VAL_TOKEM')
+
+
+erro404 = lambda: abort(404)
+r=['login','users','adduser','groups' ,'add_group', 'modify_group', 'delete_user', 'delete_group',
+    'reset_pwd', 'user_pwd',              'logout']
+
+ldap_usrs={}
+
+## erro de tokem ou login
+msgerro={'response':'Tokenize error & user login'}
+
+
+def rotas(seg):
+    s=f'/{seg}/<user>/'
+    for i in r[1:]:
+        yield s+i
+
+def deslog(app):
+    for i in r:
+        app.view_functions[i]=erro404
+    return None
+
+def limpar_rotas(app):
+    for i in r:
+        app.view_functions.pop(i)
+    return True
+
+
+def logon(user,pwd):
+    global ldap_usrs
+    user_l=user_ldap(user,pwd)
+    if user_l.connetc():
+        ldap_usrs[user]=user_l
+        return user
+    else: return False 
+
+
+def validar_tokem(func):
+    @wraps(func)
+    def new_func(user):
+        tt=request.headers.get('Authorization')
+        tokem=dict([tt.split(' ') if tt else ['a','b']])
+        bearer=tokem.get('Bearer')
+        if not val_tokem and user in ldap_usrs:   ##  <<--- passa tudo (tokem)
+            return func(user)
+        if user not in ldap_usrs or not bearer:
+            return jsonify(msgerro)
+        if ldap_usrs[user].userID==bearer:
+            return func(user)
+        return jsonify(msgerro)
+    return new_func
+
+
+def assinar():
+    assina=datetime.now().strftime("%d/%m-assinado-%d/%m")
+    dia=datetime.now().strftime("%d/%m-bolinha-%d/%m")
+    cript_dia=sha256(dia.encode('utf8')).hexdigest()
+    if liberado:
+        assina ='25/07-assinado-25/07'
+        cript_dia='0f7b8c3893290e18eaec7c09d7d794d13a83b79ab68bc3a5c228650ee87ff85f'
+    assinado=sha256((cript_dia+assina).encode('utf8')).hexdigest()
+    return cript_dia , assinado
+
+#################### paginas com autenticação ###############
+
+def pages(app,seg):
+    if r[0] in app.view_functions:
+        limpar_rotas(app)
+    rr=rotas(seg)
+
+    @app.route(next(rr),methods=['get']) ##  /users   --> pesquisa por usuario
+    @validar_tokem
+    def users(user):
+        letra=request.args.get("chr")           #letra da requisição
+        n=request.args.get("pag")               #numero da pagina
+        quant_pag=request.args.get("qp")        #quantidade por pagina  *20
+        n=int(n) if n != None else 1
+        quant_pag=int(quant_pag) if quant_pag !=None else 20 
+        pg=f'users{n:0>3}'
+        req=ldap_usrs[user].consulta(letra,quant_pag)
+        dados={'users':req.get(pg),
+                'total_pg':len(req),
+                'atual_pg':n}
+        return jsonify(dados)
+
+
+    @app.route(next(rr),methods=['POST'])  ## /adduser   --> adiciona usuario
+    @validar_tokem
+    def adduser(user):
+        dados=json.loads(request.data)
+        r=ldap_usrs[user].adduser(dados)
+        if r:
+            return r
+        return jsonify({"response":"end_function"}) 
+
+    
+    @app.route(next(rr),methods=['GET'])  ## /groups   --> pesquisa grupos
+    @validar_tokem
+    def groups(user):
+        letra=request.args.get("chr")           #letra da requisição
+        n=request.args.get("pag")               #numero da pagina
+        quant_pag=request.args.get("qp")        #quantidade por pagina  *20
+        membros=request.args.get("membros")     #deve listar os membros *0
+        n=int(n) if n != None else 1
+        quant_pag =int(quant_pag) if quant_pag !=None else 20 
+        membros= int(membros) if membros !=None else 20
+        pg=f'group{n:0>3}'
+        req=ldap_usrs[user].consulta_group(letra,quant_pag,membros)
+        dados={'groups':req.get(pg),
+                'total_pg':len(req),
+                'atual_pg':n}
+        return jsonify(dados)
+
+
+    @app.route(next(rr),methods=['POST'])   ## /add_group  -->adiciona grupo
+    @validar_tokem
+    def add_group(user):
+        dados=json.loads(request.data)
+        r=ldap_usrs[user].creat_group(dados)
+        return jsonify({'response':[False,True][r]})
+
+
+    @app.route(next(rr),methods=['POST'])   ## /modify_group   --> adiciona e remove de grupo
+    @validar_tokem
+    def modify_group(user):
+        dados=json.loads(request.data)
+        r=ldap_usrs[user].modify_group(dados)
+        return jsonify({'response':r})
+    
+
+    @app.route(next(rr),methods=['POST'])   ## /delete_user   --> delete usuario
+    @validar_tokem
+    def delete_user(user):
+        dados=json.loads(request.data)
+        r=ldap_usrs[user].rm_user(dados)
+        return jsonify({'response':r})
+
+    
+    @app.route(next(rr),methods=['POST'])   ## /delete_goup   --> delete grupo
+    @validar_tokem
+    def delete_group(user):
+        dados=json.loads(request.data)
+        r=ldap_usrs[user].rm_group(dados)
+        return jsonify({'response':r})
+    
+
+    @app.route(next(rr),methods=['POST'])   ## /reset_pwd --> trocar senha do usuario    
+    @validar_tokem
+    def reset_pwd(user):
+        dados=json.loads(request.data)
+        r=ldap_usrs[user].troca_senha(dados)
+        return jsonify({'response':r})
+    
+
+    @app.route(next(rr),methods=['POST'])   ## /user_pwd --> trocar senha de outros usuario   
+    @validar_tokem
+    def user_pwd(user):
+        dados=json.loads(request.data)
+        r=ldap_usrs[user].user_senha(dados)
+        return jsonify({'response':r})
+
+
+    #--------------------- end ---------
+    @app.route(next(rr),methods=['GET'])   ## /logout     --> desloga user
+    @validar_tokem
+    def logout(user):
+        aa=ldap_usrs.pop(user)
+        aa.logout()
+        del(aa)
+        return f'{user} logout'
+
+
+    @app.route(f'/{seg}/{r[0]}',methods=['POST']) ## /login   --> loga usuario
+    def login():
+        dados=json.loads(request.data)
+        r_user=logon(**dados)
+        if r_user:
+            dd=ldap_usrs[r_user].my_dados()
+            ldap_usrs[r_user].log_login()
+            return jsonify(dd)
+        return abort(400)
+
+
+
+#################--------##################################
+
+
+def main():
+    app=Flask(__name__)
+
+    @app.before_request
+    def before():
+        CORS(app,restore={r"/*":{"origins":f"http://{ipCORS}" , "supports_credentials":True ,  "headers": ["Authorization"]}})
+
+    data=datetime.now().strftime('%d_%m_%Y')
+    log_formatter = logging.Formatter('-'*100+'\n<hr>%(asctime)s - %(levelname)s - %(message)s -')
+    file_handler = logging.FileHandler(f'logs/erro_{data}.log')
+    file_handler.setFormatter(log_formatter)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.ERROR)
+
+
+
+    @app.route('/login/<chave>')      ## -->  loga na API
+    def index(chave):
+        global assinado, seg, cript , ldap_usrs
+        cript,assinado=assinar()
+        seg=assinado[-20:-10]
+        if chave==cript:
+            pages(app,seg)
+            return jsonify({'get':cript,'signed':assinado})
+        elif chave=='000':
+            if r[0] in app.view_functions:
+                deslog(app)
+            ldap_usrs={}
+            return f'deslogin ok '
+        else:
+            return jsonify({'get':'None','signed':'None'})
+
+
+    if logs:
+        @app.route('/log')
+        def log():
+            cript,assinado=assinar()
+            seg=assinado[-20:-10]
+            texte=open(f'logs/erro_{data}.log').read()
+            return f'''get: {cript} <br>
+            assinado: {assinado} <br>
+            rota :{seg}
+            <hr> 
+            ldap_users: {ldap_usrs}<hr>
+            rotas: {app.view_functions} <hr>
+            {texte}
+            '''
+
+
+    @app.route('/doc')   ## --> documentação da API
+    def doc():
+        return render_template('doc.html')
+    
+    app.run(host=host,port=port)
+
+
+######### app   ----------------------  ######
+
+
+if __name__ == '__main__':
+    main()
---
+++ clildap.py
@@ -0,0 +1,233 @@
+from ldap3 import Server, Connection, ALL, SUBTREE , MODIFY_ADD , MODIFY_DELETE
+import subprocess as sub
+from hashlib import sha256
+from random import randint
+from json import loads
+from datetime import datetime 
+
+with open('config.ini') as arq:
+    conf=loads(arq.read())
+    arq.close()
+
+host=conf.get('app_api').get('HOST_LDAP')
+dominio=conf.get('app_api').get('DOMINIO')
+base=conf.get('app_api').get('BASE')
+tokem=conf.get('testes').get('FAKE_TOKEM')
+val_tokem=conf.get('testes').get('VAL_TOKEM')
+shell=conf.get('testes').get('SHELL')
+
+
+def valid(dd):
+    return f'{dd}'.replace("[]",'')
+
+class user_ldap:
+    def __init__(self,user,pwd):
+        self.user=user
+        self.logon=f'{user}{dominio}'
+        self.pwd=pwd
+        self.chave=f'{randint(0,33**333)}'
+        self.userID= sha256((user+self.chave).encode()).hexdigest() if val_tokem else tokem
+
+    
+    def log_login(self):
+        data=datetime.now().strftime("%d_%m_%Y")
+        hora=datetime.now().strftime("%H:%M")
+        with open(f'./login/login_{data}.log','a') as arq:
+            texto=f'''"{self.user}" acesso em {hora}\n'''
+            arq.write(texto)
+            arq.close()
+
+    def connetc(self):
+        self.server=Server(host,get_info=ALL) 
+        self.conn=Connection(self.server,self.logon,self.pwd)
+        return self.conn.bind()
+    
+    def logout(self):
+        self.conn.unbind()
+        return True
+    
+    def my_dados(self):
+        self.conn.search(f'{base}',
+                        f'(&(objectclass=user)(sAMAccountName={self.user}))' ,
+                        attributes=['cn','sAMAccountName','distinguishedName','memberof','mail','givenname'],
+                        search_scope=SUBTREE)
+        i=self.conn.entries[0]
+        self.all_dados={'user':valid(f'{i.cn}'),
+                'givenname': valid(f'{i.givenname}'),
+                'memberof': valid(f'{i.memberof}'),
+                'sAMAccountName': valid(f'{i.sAMAccountName}'),
+                'mail2': valid(f'{i.mail}'),
+                'mail': valid(f'{i.sAMAccountName}')+dominio,
+                'givenname': valid(f'{i.givenname}'),
+                'token':self.userID,
+                'DN': f'{i.distinguishedName}'
+                }
+        return self.all_dados
+    
+    def consulta(self,nome,quant_pag=20):
+        text=f'(&(objectclass=user)(cn={nome}*))'
+        ll=['displayname','cn','givenname','mail','telephonenumber','objectclass','memberof','distinguishedName']
+        self.conn.search(base, text ,attributes=ll)
+        dados={}
+        cont=1
+        for n,i in enumerate(self.conn.entries):
+            if n%quant_pag==0:
+                NL=f'users{cont:0>3}'
+                dados.update({NL:[]})
+                pag=dados[NL]
+                cont+=1
+            pag.append({
+                'cn':f'{i.cn}'.replace("[]",''),
+                'givenname': f'{i.givenname}'.replace("[]",''),
+                'memberof': f'{i.memberof}'.replace("[]",''),
+                'telephonenumber': f'{i.telephonenumber}'.replace("[]",''),
+                'mail': f'{i.mail}'.replace("[]",''),
+                'givenname': f'{i.givenname}'.replace("[]",''),
+                'DN': f'{i.distinguishedName}'
+                })
+        return dados
+    
+    def adduser(self,dados):
+        nome=dados['nome']      # dados
+        l_nome=nome.split()
+        fn=l_nome[0]
+        ln=' '.join(l_nome[1:])
+        login1=f'{fn}.{l_nome[-1]}'
+        login2=f'{fn}.{[l_nome[-3],l_nome[-2]][len(l_nome[-2])>2]}'
+
+        DN=f"CN={nome},CN=Users,{base}"
+        attr={
+            'objectclass':['top','person', 'organizationalPerson', 'user'],
+            'cn':nome,
+            'displayname':nome,
+            'uid':login1,
+            'givenName':l_nome[0],
+            'sn':' '.join(l_nome[1:]),
+            'sAMAccountName':login1,
+            'description':dados["desc"],
+            'mail':dados.get('email2'),
+            'userPrincipalName':f'{login1}@ufnt.local',
+            'userAccountControl':'66080' }
+        r=self.conn.add(DN,attributes=attr)
+        c=login1
+        b='usuario criado'
+        if self.conn.result['result']==19:
+            c=login2
+            attr.update({'userPrincipalName':f'{login2}@ufnt.local',
+                        'uid':login2,
+                        'sAMAccountName':login2,
+                        })
+            r=self.conn.add(DN,attributes=attr)
+            if not r:
+                c=f'{login1} & {c}'
+                b='o login ja existe'
+        if r:
+            command=f'dsmod user "{DN}" -pwd "{dados["pwd"]}" -mustchpwd no'
+            a='passa sem shell'
+
+            if shell:
+                a=sub.run(command,shell=1,capture_output=1,text=1).stdout
+            if a=='':
+                b='erro no formato da senha'
+                self.conn.delete(DN)
+                r=False
+        if self.conn.result['result']==68:
+            b='Usuario ja existe'
+        response={'response':r,'mensg':b,'login':c}
+        return response
+    
+    def rm_user(self,dados):
+        DN=dados.get('DN')
+        if DN==self.all_dados["DN"]:
+            return None
+        self.conn.delete(DN)
+        if self.conn.result['result']==0:
+            return True
+        return False
+
+    def consulta_group(self,letra,quant_pag=20,membros=0):
+        text=f'(&(objectclass=group)(cn={letra}*))'
+        ll=['cn','distinguishedName','desktopProfile']
+        dados={}
+        cont=1
+        if membros:ll.append('member')
+        self.conn.search(base, text ,attributes=ll)
+        for n,i in enumerate(self.conn.entries):
+            if n%quant_pag==0:
+                NL=f'group{cont:0>3}'
+                dados.update({NL:[]})
+                pag=dados[NL]
+                cont+=1
+            ugp=valid(f'{i.desktopProfile}')
+            dici={  'cn':valid(f'{i.cn}'),
+                    'DN':f'{i.distinguishedName}',
+                    'UGP':ugp.split()[-1] if ugp else ''}
+            if membros:
+                dici['membros']=[j.split(',')[0].split('=')[1] for j in i.member]
+            pag.append(dici)
+        return dados
+
+    def creat_group(self, dados):
+        nome=dados['nome']
+        UGP=dados['UGP']
+        dn=f'CN={nome},CN=users,{base}'
+        desc='grupo de teste LDAP3'
+        group={
+            'objectclass':['top','group'],
+            'cn':nome,
+            'description':desc,
+            'desktopProfile':f'UGP {UGP}',
+            }
+        r=self.conn.add(dn,attributes=group)
+        return r
+    
+    def modify_group(self,dados):
+        user_dn=dados['DN_user']
+        group_dn=dados['DN_group']
+        acao={  'add': MODIFY_ADD,
+                'remove': MODIFY_DELETE} [dados['modify']]
+        
+        add={'member' : [(acao, [user_dn])]}
+        self.conn.modify(group_dn, add)
+        if self.conn.result['result']==0:
+            return True
+        return False
+
+    def rm_group(self,dados):
+        DN=dados['DN']
+        self.conn.delete(DN)
+        return self.conn.result['result']==0
+
+    def troca_senha(self,dados):
+        pwd=dados['pwd']
+        new_pwd=dados['new_pwd']
+        DN=self.all_dados['DN']
+        con=Connection(self.server,self.logon,pwd)
+        if not con.bind(): 
+            return {'response':False , 'mensg':'senha antiga invalida'}
+        command=f'dsmod user "{DN}" -pwd "{new_pwd}" -mustchpwd no'
+        a='passa sem shell'
+        if shell:
+            a=sub.run(command,shell=1,capture_output=1,text=1).stdout
+            a=sub.run(command,shell=1,capture_output=1,text=1).stdout
+            pass
+        if a=='':
+            return {'response':False , 'mensg':'nova senha invalida'}
+        return {'response':True , 'mensg':'ok'}
+
+    def user_senha(self,dados):
+        DN=dados.get('DN')
+        new_pwd=dados.get("new_pwd")
+        command=f'dsmod user "{DN}" -pwd "{new_pwd}" -mustchpwd no'
+        a='passa sem shell'
+        print(command)
+        if shell:
+            a=sub.run(command,shell=1,capture_output=1,text=1).stdout
+            a=sub.run(command,shell=1,capture_output=1,text=1).stdout
+            pass
+        if a=='':
+            return {'response':False , 'mensg':'nova senha invalida'}
+        return {'response':True , 'mensg':'ok'}
+        
+        
+
